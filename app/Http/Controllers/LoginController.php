b509c55f368beaa2d1940be985852d251a1dcc0f
etinurkholifah <etinurkholifah67@gmail.com>
2024-03-07 15:40:13 +0700

    perubahan

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('auth.login');
+    }
+
+    public function login_proses(Request $request){
+        // dd($request->all());
+        $request->validate([
+            'email'    => 'required',
+            'password'=> 'required',
+
+        ]);
+
+        $data = [
+            'email'     =>$request->email,
+            'password'  =>$request->password
+        ];
+        if (Auth::attempt($data)){
+            return redirect()->route('dashboard');
+        }else{
+            return redirect()->route('login')->with('failed','Email atau password salah');
+        }
+    }
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login')->with('succes', 'kamu berhasil logout');
+    }
+    public function register(){
+        return view('auth.register');
+    }
+    public function register_proses(Request $request){
+        $request -> validate([
+            'nama'      => 'required',
+            'email'     => 'required|email|unique:users,email',
+            'password'  =>  'required|min:6'
+        ]);
+
+        $data['name'] = $request->nama;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+
+        User::create($data);
+
+        
+        $login = [
+            'email'     =>$request->email,
+            'password'  =>$request->password
+        ];
+        if (Auth::attempt($login)){
+            return redirect()->route('dashboard');
+        }else{
+            return redirect()->route('login')->with('failed','Email atau password salah');
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/login',[App\Http\Controllers\LoginController::class,'index'])->name('login');
+Route::post('/login-proses',[App\Http\Controllers\LoginController::class,'login_proses'])->name('login-proses');
+Route::get('/logout',[App\Http\Controllers\LoginController::class,'logout'])->name('logout');
+Route::get('/register',[App\Http\Controllers\LoginController::class,'register'])->name('register');
+Route::post('/register-proses',[App\Http\Controllers\LoginController::class,'register_proses'])->name('register-proses');
+
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'dashboard'])->name('dashboard');
+Route::get('/user',[App\Http\Controllers\HomeController::class,'index'])->name('index');
+
+Route::get('/create',[App\Http\Controllers\HomeController::class,'create'])->name('user.create');
+Route::post('/store',[App\Http\Controllers\HomeController::class,'store'])->name('user.store');
+
+Route::get('/edit/{id}',[App\Http\Controllers\HomeController::class,'edit'])->name('user.edit');
+Route::put('/update/{id}',[App\Http\Controllers\HomeController::class,'edit'])->name('user.update');
+
+
+
+
+// Route::get('/user', [\App\Http\Controllers\HomeController::class, 'index'])->name('index');
+//Route::get('/user',[HomeController::class,'index']);
